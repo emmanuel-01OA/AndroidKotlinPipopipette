9c06d09ffc9d35093cfdbb658f795d1665de9cea
emmanuel-01OA <assoko0005@gmail.com>
2023-09-03 13:03:39 +0000

    Initial commit

---
+++ app/src/main/java/com/example/androidkotlinpipopipette/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.androidkotlinpipopipette
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btnJouer)
+        button.setOnClickListener {
+
+            val intent: Intent = Intent(this, Menuconfig::class.java);
+            this.startActivity(intent)
+            finish()
+
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/androidkotlinpipopipette/Menuconfig.kt
@@ -0,0 +1,18 @@
+package com.example.androidkotlinpipopipette
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+
+class Menuconfig : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menuconfig)
+
+       // val linearLayout: LinearLayout = findViewById(R.id.menupipo)
+
+
+    }
+}
