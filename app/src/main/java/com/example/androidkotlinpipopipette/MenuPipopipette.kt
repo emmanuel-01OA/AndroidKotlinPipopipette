a8debef027a5136b43972926854ff133dc85cdf7
TIE WILLY <Pikasso.wilfried@gmail.com>
2023-09-11 17:13:07 +0200

    construction de la page main jeu pipopipette

--- app/src/main/java/com/example/androidkotlinpipopipette/MenuPipopipette.kt
+++ app/src/main/java/com/example/androidkotlinpipopipette/MenuPipopipette.kt
@@ -1,23 +1,11 @@
 package com.example.androidkotlinpipopipette
 
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 
-
-class MainActivity : AppCompatActivity() {
+class MenuPipopipette : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-
-
-
-        }
-
+        setContentView(R.layout.activity_menu_pipopipette)
+    }
 }
--- app/src/main/java/com/example/androidkotlinpipopipette/PageHelpPipopipette.kt
+++ app/src/main/java/com/example/androidkotlinpipopipette/PageHelpPipopipette.kt
@@ -1,23 +1,11 @@
 package com.example.androidkotlinpipopipette
 
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 
-
-class MainActivity : AppCompatActivity() {
+class PageHelpPipopipette : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-
-
-
-        }
-
+        setContentView(R.layout.activity_page_help_pipopipette)
+    }
 }
